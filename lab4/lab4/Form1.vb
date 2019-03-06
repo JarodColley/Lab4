70148b88480b8143f370777277c7125da57c3efa
JarodColley <47607281+JarodColley@users.noreply.github.com>
2019-03-06 13:09:19 -0500

    Added main programming for enter button

--- lab4/lab4/Form1.vb
+++ lab4/lab4/Form1.vb
@@ -15,7 +15,87 @@ Public Class FrmMain
         Dim carItem As ListViewItem    ' declares a ListViewItem class
 
 
+        ' validate the data in the form
+        If IsValidInput() = True Then
+
+            ' set the edit flag to true
+            editMode = True
+
+            ' 
+            lblResult.Text = "It worked!"
+
+            ' if the current customer identification number has a no value
+            ' then this is not an existing item from the listview
+            If currentCarIdentificationNumber.Trim.Length = 0 Then
+
+                ' create a new car object using the parameterized constructor
+                car = New Car(cmbMake.Text, txtModel.Text, Convert.ToInt32(cmbYear.Text), Convert.ToInt32(txtPrice.Text), chkNew.Checked)
+
+                ' add the customer to the customerList collection
+                ' using the identification number as the key
+                ' which will make the customer object easier to
+                ' find in the customerList collection later
+                carList.Add(car.Id.ToString(), car)
+
+            Else
+                ' if the current customer identification number has a value
+                ' then the user has selected something from the list view
+                ' so the data in the customer object in the customerList collection
+                ' must be updated
+
+                ' so get the customer from the custmers collection
+                ' using the selected key
+                car = CType(carList.Item(currentCarIdentificationNumber), Car)
+
+                ' update the data in the specific object
+                ' from the controls
+                car.Make = cmbMake.Text
+                car.Model = txtModel.Text
+                car.Year = Convert.ToInt32(cmbYear.Text)
+                car.Price = Convert.ToInt32(txtPrice.Text)
+                car.Status = chkNew.Checked
+            End If
+
+            ' clear the items from the listview control
+            lvwCustomers.Items.Clear()
+            ' loop through the customerList collection
+            ' and populate the list view
+            For Each carEntry As DictionaryEntry In carList
+
+                ' instantiate a new ListViewItem
+                carItem = New ListViewItem()
+
+                ' get the customer from the list
+                car = CType(carEntry.Value, Car)
+
+                ' assign the values to the checked control
+                ' and the subitems
+                carItem.Checked = car.Status
+                carItem.SubItems.Add(car.Id.ToString())
+                carItem.SubItems.Add(car.Make)
+                carItem.SubItems.Add(car.Model)
+                carItem.SubItems.Add(car.Year.ToString())
+                carItem.SubItems.Add(car.Price.ToString())
+
+
+
+                ' add the new instantiated and populated ListViewItem
+                ' to the listview control
+                lvwCustomers.Items.Add(carItem)
+
+            Next carEntry
+
+
+            ' clear the controls
+            Reset()
+
+            ' set the edit flag to false
+            editMode = False
+
+        End If
+
     End Sub
+
     ''' <summary>
     ''' IsValidInput - validates the data in each control to ensure that the user has entered apprpriate values
     ''' </summary>
